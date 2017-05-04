e5170990a2c7cefb1fa7368426e7316b9d3fce53
daniel4993 <daniel4993@gmail.com>
2017-05-04 10:39:47 -0400

    Add files via upload

---
+++ model/fixed_fluid_density_fixed_droplet_radius_position_infer_drag_scenario.C
@@ -0,0 +1,114 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// DIP - Droplet Inverse Problem
+//
+// Copyright (C) 2017 Paul T. Bauman, Matthew Ringuette, David Salac
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2 GNU General
+// Public License as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor, Boston, MA
+// 02110-1301 USA
+//
+//--------------------------------------------------------------------------
+//-----------------------------------------------------------------------el-
+
+// This class
+#include <dip/fixed_fluid_density_fixed_droplet_radius_position_infer_drag_scenario.h>
+
+// DIP
+#include <dip/droplet_data.h>
+#include <dip/droplet_likelihood.h>
+
+// QUESO
+#include <queso/Defines.h>
+#include <queso/getpot.h>
+#include <queso/GslVector.h>
+#include <queso/GslMatrix.h>
+#include <queso/VectorSpace.h>
+#include <queso/BoxSubset.h>
+#include <queso/UniformVectorRV.h>
+#include <queso/1DQuadrature.h>
+#include <queso/TensorProductQuadrature.h>
+
+// C++
+#include <sstream>
+
+namespace DIP
+{
+
+  template<typename VectorType, typename MatrixType>
+  FixedFluidDensityFixedDropletRadiusPositionInferDragScenario<VectorType,MatrixType>::
+  FixedFluidDensityFixedDropletRadiusPositionInferDragScenario( const QUESO::GetPot & input,
+                                                               const QUESO::BaseEnvironment & queso_env,
+                                                               const DropletData<VectorType,MatrixType> & data )
+    : FixedFluidDensityFixedDropletRadiusPositionScenarioBase<VectorType,MatrixType>(input,data),
+    //_n_params(1+this->_data.n_data())
+     _n_params(2)
+    //_n_marg_params(1)
+  {
+    // Setup parameter space, domain, and RV
+    this->_param_space.reset( new QUESO::VectorSpace<VectorType,MatrixType>(queso_env,
+                                                                            "param_space_",
+                                                                            this->_n_params,
+                                                                            NULL) );
+
+    std::unique_ptr<VectorType> param_mins( this->_param_space->newVector() );
+    std::unique_ptr<VectorType> param_maxs( this->_param_space->newVector() );
+
+    (*param_mins)[0] = this->_rho_d_min;
+    (*param_mins)[1] = this->_cd_min;
+
+    (*param_maxs)[0] = this->_rho_d_max;
+    (*param_maxs)[1] = this->_cd_max;
+
+    this->_param_domain.reset( new QUESO::BoxSubset<VectorType,MatrixType>("param_domain_", *(this->_param_space),
+                                                                           *param_mins,*param_maxs) );
+
+    this->_prior_rv.reset( new QUESO::UniformVectorRV<VectorType,MatrixType>("prior_rv_",
+                                                                             *(this->_param_domain)) );
+
+    this->_likelihood = DropletLikelihood<VectorType,MatrixType>::build_no_marg(*this);
+  }
+
+  template<typename VectorType, typename MatrixType>
+  void
+  FixedFluidDensityFixedDropletRadiusPositionInferDragScenario<VectorType,MatrixType>::
+  eval_model( const VectorType & domain_vector,
+              VectorType & model_output ) const
+  {
+    queso_assert_equal_to(domain_vector.sizeGlobal(), this->_n_params);
+   // queso_assert_equal_to(marg_vector.sizeGlobal(), this->_n_marg_params);
+    queso_assert_equal_to(model_output.sizeGlobal(), this->_data.n_data());
+
+    const double rho_d = this->_rho_d_nom*domain_vector[0];
+    const double cd = this->_cd*domain_vector[1];
+
+
+   for( unsigned int i = 0; i < this->_data.n_data(); i++ )
+      {
+        const double t = this->_data.time(i);
+
+        const double r_d = this->_data.droplet_radius(i);
+
+        model_output[i] = this->_model.position(this->_rho_f,
+                                                rho_d,
+                                                r_d,
+                                                cd,
+                                                t);
+      }
+  }
+
+} // end namespace DIP
+
+// Instantiate
+template class DIP::FixedFluidDensityFixedDropletRadiusPositionInferDragScenario<QUESO::GslVector,QUESO::GslMatrix>;
